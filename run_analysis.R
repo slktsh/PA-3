231088da12a681d6050258e4cd9949e7100d3e78
slktsh <slktsh@users.noreply.github.com>
2015-05-24 03:16:20 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -24,11 +24,24 @@ index_std<- grep("std", names_all, ignore.case = TRUE)
 ## an integer vector with indices of the elements, containing "std" case-insensitive
 names_std<- grep("std", names_all, ignore.case = TRUE, value=TRUE)
 ## a character vector with the names of the variables containing "std" case-insensitive
-																
+
 
 index<-c(index_mean, index_std) # a combined index for the columns, containing "mean" or "std" in their names
-names<-c("subject", "activities", names_mean, names_std) # the same for the names
+names<-c("subject", "activity", names_mean, names_std) # the same for the names
 data<-data[index] # only the columns, containing "mean" or "std" in their names are left
+
+# here we should modify the names vector using the descriptive and more readable variable names
+names <- gsub('\\(|\\)',"", names, perl = TRUE)
+names <- gsub('Acc',"Acceleration",names)
+names <- gsub('GyroJerk',"AngularAcceleration",names)
+names <- gsub('Gyro',"AngularSpeed",names)
+names <- gsub('Mag',"Magnitude",names)
+names <- gsub('^t',"TimeDomain.",names)
+names <- gsub('^f',"FrequencyDomain.",names)
+names <- gsub('\\.mean',".Mean",names)
+names <- gsub('\\.std',".StandardDeviation",names)
+names <- gsub('Freq\\.',"Frequency.",names)
+names <- gsub('Freq$',"Frequency",names)
 
 act_vect<- act[ ,1] # activities vector
 act_vect<- as.vector(act_vect) # activities vector of integers
@@ -47,14 +60,14 @@ names(data)<- names # assigns the names to the data set
 library(dplyr) # calls dplyr package
 
 tbl<- tbl_df(data) # converts data set into a format suitable for dplyr-manipulations
-tbl_s4<-arrange(tbl, subject, activities) 
+tbl_s4<-arrange(tbl, subject, activity) 
 # arranges the columns in ascending order by subject, then by the activity label
 # tbl_s4 is a table required in the Step 4
 
-tbl_s4<- group_by(tbl_s4, subject, activities) # groups tbl_s4 by subject and then by activities
+tbl_s4<- group_by(tbl_s4, subject, activity) # groups tbl_s4 by subject and then by activities
 tbl_s5<-summarise_each(tbl_s4, funs(mean))     # computes mean() for each group
 names<-names(tbl_s5)[3:length(names)]
 new_names<-paste("Mean(", names, ")", sep="")  # creates a vector with the correct names
-names(tbl_s5)<-c("subject-ID", "activity-ID", new_names) 
+names(tbl_s5)<-c("subject", "activity", new_names) 
 # assigns the columns the correct names now tbl_s5 is a tbl required in the Step 5
 write.table(tbl_s5, file = "tbl_s5.txt", row.name=FALSE, col.names = TRUE)
